122ebda02857a28280481646c2af562becf8615f
Jayesh Ikhar <jayeshikhar01@gmail.com>
2025-01-11 01:41:06 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// require("dotenv").config({path: './env'});
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2nd approach : where we used iife ()() with try catch and async await if the database lies in another continent. But this is not a good approach, we should create a seperate file (we will do in approach one)
+
+/*
+import express from 'express'
+import mongoose from "mongoose";    
+import { DB_NAME } from "./constants";
+
+const app = express();
+(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+       app.on("error", (error) => {
+        console.log("Error: ",error)
+       })
+
+       app.listen(process.env.PORT, () => {
+           console.log(`App is listening on port ${process.env.PORT}`)
+       })
+    } catch (error) {
+        console.log("Error: ",error)
+        throw error
+    }
+}) ()
+    */
